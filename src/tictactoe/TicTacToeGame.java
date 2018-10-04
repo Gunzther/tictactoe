a87f7d5f89034090bc632f71567e838c67088a5b
KameriiJ <jenny.sirarat@gmail.com>
2018-10-04 15:14:29 +0700

    fixed errors

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -6,11 +6,11 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.Node;
 
-/**		
-* This class contain logic of the tictactoe game.		
-* 		
-* @author jim		
-*/
+/**
+ * This class contain logic of the tictactoe game.
+ * 
+ * @author jim
+ */
 public class TicTacToeGame {
 	private final int boardsize;
 	/** View of the TicTacToe board. */
@@ -19,70 +19,81 @@ public class TicTacToeGame {
 	private Piece[][] pieces;
 	/** Flag for game over. An observable object. */
 	private SimpleBooleanProperty gameOver;
-	
+
 	private Player nextPlayer = Player.X;
 	
+	private int count = 0 ;
+
 	public TicTacToeGame(int size) {
 		this.boardsize = size;
-		board = new Board(boardsize,boardsize);   // view of the gameboard
+		board = new Board(boardsize, boardsize); // view of the gameboard
 		pieces = new Piece[boardsize][boardsize]; // stores info about pieces on board
 		gameOver = new SimpleBooleanProperty(false);
 		startNewGame();
 	}
-	
+
 	public Board getBoard() {
 		return board;
 	}
-	
+
 	public void startNewGame() {
 		// Avoid nulls. Assign a "none" object to each location on the board.
-		for(int row=0; row<boardsize; row++) 
-			for(int col=0; col<boardsize; col++) pieces[row][col] = Piece.NONE;
-		// Remove Pieces from the board (view), but not the squares themselves. Use a Predicate to test for Piece.
+		for (int row = 0; row < boardsize; row++)
+			for (int col = 0; col < boardsize; col++)
+				pieces[row][col] = Piece.NONE;
+		// Remove Pieces from the board (view), but not the squares themselves. Use a
+		// Predicate to test for Piece.
 		Predicate<Node> isPiece = (node) -> node instanceof Piece;
 		board.getChildren().removeIf(isPiece);
 		gameOver.set(false);
 	}
-	
+
 	/**
 	 * Test whether a player can move to a square.
+	 * 
 	 * @return true if can move to the requested (col,row) on board.
 	 */
 	public boolean canMoveTo(Player player, int col, int row) {
-		if (row<0 || row>pieces.length) return false;
-		//no moves allowed after the game end!
-		if (col<0 || col>pieces[row].length) return false;
-		//check if the square is empty
-		//no move allowed after game is over
-		if(isGameOver())return false;
+		if (row < 0 || row > pieces.length)
+			return false;
+		// no moves allowed after the game end!
+		if (col < 0 || col > pieces[row].length)
+			return false;
+		// check if the square is empty
+		// no move allowed after game is over
+		if (isGameOver())
+			return false;
 		return pieces[row][col] == null || pieces[row][col] == Piece.NONE;
 	}
-	
+
 	/**
-	 * Place a piece at a given (row,col) on the game board.
-	 * It is up to the caller to make sure that the cell can
-	 * be occupied before calling moveTo.
+	 * Place a piece at a given (row,col) on the game board. It is up to the caller
+	 * to make sure that the cell can be occupied before calling moveTo.
 	 * 
-	 * @param piece the piece to place 
-	 * @param row board row to move to
-	 * @param col board column to move to
+	 * @param piece the piece to place
+	 * @param row   board row to move to
+	 * @param col   board column to move to
 	 */
 	public void moveTo(Piece piece, int col, int row) {
-		assert canMoveTo(piece.type, col, row): 
-			String.format("moveTo(%s,%d,%d) is invalid",piece.toString(),row,col);
-		if (! canMoveTo(piece.type, col, row) ) return; // not reached when assertions enabled
+		assert canMoveTo(piece.type, col, row) : String.format("moveTo(%s,%d,%d) is invalid", piece.toString(), row,
+				col);
+		if (!canMoveTo(piece.type, col, row))
+			return; // not reached when assertions enabled
 		pieces[row][col] = piece;
 		board.add(piece, col, row); // GridPane.add has column param before row param
-		
+
 		/** next player's turn to move. */
-		if (piece.type == Player.X) nextPlayer = Player.O;
-		else nextPlayer = Player.X;
+		if (piece.type == Player.X)
+			nextPlayer = Player.O;
+		else
+			nextPlayer = Player.X;
 		/** after each move check if board is full */
 		/** and check if someone won the game. */
-		if (boardIsFull() || winner() != Player.NONE) gameOver.set(true);
-		
+		if (boardIsFull() || winner() != Player.NONE)
+			gameOver.set(true);
+
 	}
-	
+
 	/**
 	 * Evaluate board to see if a player has won.
 	 * @return reference to Player that wins. If no winner returns Player.NONE.
@@ -90,7 +101,6 @@ public class TicTacToeGame {
 	public Player winner() {
 		// This is fast but hacky and only works when the
 		// number of pieces in a line to win (3) equals board size (3).
-		
 		// Look for N matching pieces on same row.
 		rowtest:
 		for(int row=0; row<boardsize; row++) {
@@ -113,50 +123,69 @@ public class TicTacToeGame {
 			return p;
 		}
 		// Look for N matching pieces on downward diagonal.
+		count = 0 ;
 		Player p = pieces[0][0].type;
-		if (p != Player.NONE && p == pieces[1][1].type && p == pieces[2][2].type && p == pieces[3][3].type) {
-			// all pieces on diagonal occupied by same type (Player)
-			return p;
+		if (p != Player.NONE ) {
+			for ( int i = 1 ; i < boardsize ; i++ ) {
+				if( p == pieces[i][i].type) {
+					count++; 
+				}
+			}
+				if ( count == boardsize-1) {
+					return p;
+				}
 		}
+		count = 0 ;
 		// Look for N matching pieces on upward diagonal
-		p = pieces[0][2].type; // start at lower-left corner
-		if (p != Player.NONE && p == pieces[1][1].type && p == pieces[2][0].type && p == pieces[3][0].type) {
-			// all pieces on diagonal occupied by same type (Player)
-			return p;
+		p = pieces[0][boardsize-1].type; // start at lower-left corner
+		if (p != Player.NONE ) {
+			for ( int j = 1 ; j < boardsize ; j++ ) {
+				if( p == pieces[j][boardsize-(j+1)].type) {
+					count++; 
+			}
+				if ( count == boardsize-1) {
+					return p;
+				}
+			}
 		}
 		return Player.NONE;
 	}
-	
+
 	/**
 	 * Get the value of gameOver status.
+	 * 
 	 * @return true if game is over.
 	 */
 	public boolean isGameOver() {
 		return gameOver.get();
 	}
-	
+
 	/**
-	 * Get the GameOver property.
-	 * This enables adding observers to the gameOver property.
+	 * Get the GameOver property. This enables adding observers to the gameOver
+	 * property.
+	 * 
 	 * @return gameOver property
 	 */
 	public BooleanProperty gameOver() {
 		return gameOver;
 	}
-	
+
 	/** Get player who's turn it is to move. */
 	public Player getNextPlayer() {
 		return nextPlayer;
 	}
-	
-	/** 
+
+	/**
 	 * Check if game board is fully occupied.
+	 * 
 	 * @return true if board is full
 	 */
 	public boolean boardIsFull() {
 		/** check if board is full */
-		for(int row=0; row<boardsize; row++) {
-			for(int col=0; col<boardsize; col++) if (pieces[row][col] == Piece.NONE) return false;
+		for (int row = 0; row < boardsize; row++) {
+			for (int col = 0; col < boardsize; col++)
+				if (pieces[row][col] == Piece.NONE)
+					return false;
 		}
 		return true;
 	}
